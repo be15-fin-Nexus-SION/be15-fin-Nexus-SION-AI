2dc832f5060123b2e5c0b385d49589772c8ab40e
develup_psy <develup_psy@github.com>
2025-07-14 09:54:39 +0900

    refactor: Qdrant 모듈화

--- app/chains/retriever.py
+++ app/chains/retriever.py
@@ -1,15 +1,4 @@
-from langchain_community.vectorstores import Qdrant
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from qdrant_client import QdrantClient
-
-embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-client = QdrantClient(host="localhost", port=6333)
-
-vectorstore = Qdrant(
-    client=client,
-    collection_name="fp_examples",
-    embeddings=embedding
-)
+from app.chains.vector_store import vectorstore
 
 retriever = vectorstore.as_retriever(
     search_type="similarity_score_threshold",
